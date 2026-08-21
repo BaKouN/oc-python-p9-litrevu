b4e923b0405931c2a0e6811670800ebb360b0321
Haroun Bakhouche <haroun@revolucy.fr>
2026-08-22 01:28:08 +0200

    Add Ticket, Review, UserFollows models + admin registration

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Ticket, Review, UserFollows
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Ticket)
+admin.site.register(Review)
+admin.site.register(UserFollows)
